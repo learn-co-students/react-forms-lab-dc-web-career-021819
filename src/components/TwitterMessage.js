d423918a83e916878a1691e765f4db00fdb3b381
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-24 10:22:16 -0400

    add solution

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,37 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      form: {msg: "", counter: 0},
+    };
+  }
+
+  handleChangeInput = event => {
+    const maxLength = this.props.maxChars
+    const value1 = event.target.value;
+    const msg = value1.slice(0, maxLength);
+    const counter = msg.length
+    this.setState({
+      form: {
+        msg,
+        counter
+      }
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={this.handleChangeInput}
+          value={this.state.form.msg}
+          maxLength={this.props.maxChars}
+          />
+        <span> {this.state.form.msg.length}/{this.props.maxChars}</span> <span> {(this.props.maxChars) - (this.state.form.msg.length)}</span>
       </div>
     );
   }
